445475d065e8f7a29de31869bc2c2b1630ba4769
Departamento de Sistemas <sistemas@azc.uam.mx>
2024-04-26 07:40:31 -0600

    Modificacion en URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	base: '/potrones-dis',
+	base: '/patrones-dis',
 	integrations: [
 		starlight({
 			title: 'PDS',
